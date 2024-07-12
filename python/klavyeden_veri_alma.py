87c12ea4651e04e1a3de9f7e4aa4fddb518a05e2
tunahanbilgic <tunahnb@gmail.com>
2024-07-12 09:04:47 +0300

    kodlama

---
+++ python/klavyeden_veri_alma.py
@@ -0,0 +1,6 @@
+isim = input('Adınızı girin: ')
+yaş = input('Yaşınızı girin: ') #str halde
+boş = ""                        #False değeri verecek.
+print(type(isim), type(yaş))
+print(list(isim), set(isim), tuple(isim), bool(isim), bool(boş))
+print(int(yaş), int(yaş, 7), float(yaş), hex(int(yaş)), complex(yaş))
